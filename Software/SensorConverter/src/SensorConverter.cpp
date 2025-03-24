841a2527dc02cf75cb2c2ab8ab743964e14d7e31
Joey Strandnes <joey.strandnes@hotmail.com>
2025-03-24 14:07:38 +0100

    Still some issues with NVM

--- Software/SensorConverter/src/SensorConverter.cpp
+++ Software/SensorConverter/src/SensorConverter.cpp
@@ -9,6 +9,7 @@
 
 //FIXME
 extern CRC_HandleTypeDef hcrc;
+extern I2C_HandleTypeDef hi2c1;
 
 
 SensorConverterSettings::SensorConverterSettings(
@@ -98,8 +99,17 @@ void SensorConverterSettings::GetSettingsFromEEPROM(){
 	uint16_t SettingsSize = sizeof(*this);
 
 	//Step 1: Read the data from the EEPROM.
-	HAL_I2C_Master_Transmit(I2C, 0xA0, MemoryAddress, 2, 100);
-	HAL_I2C_Master_Receive(I2C, 0xA0, (uint8_t *)this, SettingsSize, 100);
+	HAL_NVIC_EnableIRQ(I2C1_EV_IRQn);
+
+	HAL_I2C_Master_Seq_Transmit_IT(&hi2c1, 0xA0, MemoryAddress, 2, I2C_FIRST_AND_NEXT_FRAME);
+
+	HAL_Delay(1); //HAL_I2C_GetState(I2C) != HAL_I2C_STATE_BUSY_TX
+
+	HAL_I2C_Master_Seq_Receive_IT(&hi2c1, 0xA0, (uint8_t *)this, SettingsSize, I2C_LAST_FRAME);
+
+	HAL_Delay(100); //while(HAL_I2C_GetState(I2C) == HAL_I2C_STATE_BUSY_RX);
+
+
 
 	//Step 2: Calculate the checksum of the first 12 bytes of the Settings.
 	uint8_t TempBuffer[12];
@@ -128,17 +138,17 @@ void SensorConverterSettings::WriteSettingsToEEPROM(){
 	MemoryAddress[0] = 0;
 	MemoryAddress[1] = 0;
 
-	HAL_I2C_Master_Abort_IT(I2C, 0xA0);
-	while(HAL_I2C_GetState(I2C) != HAL_I2C_STATE_READY && HAL_I2C_GetState(I2C) != HAL_I2C_STATE_RESET);
+	HAL_I2C_Master_Seq_Transmit_IT(&hi2c1, 0xA0, MemoryAddress, 2, I2C_FIRST_AND_NEXT_FRAME);
 
-	HAL_I2C_Master_Seq_Transmit_IT(I2C, 0xA1, MemoryAddress, 2, I2C_FIRST_AND_NEXT_FRAME);
+	HAL_Delay(1);
 
 	//Yes this is very stupid, it is important that this happens before the rest of the system is initiated.
 	//ST HAL does not offer a "sequenced" transmit in blocking mode...
-	while(HAL_I2C_GetState(I2C) != HAL_I2C_STATE_READY && HAL_I2C_GetState(I2C) != HAL_I2C_STATE_RESET);
+	//while(HAL_I2C_GetState(I2C) != HAL_I2C_STATE_READY && HAL_I2C_GetState(I2C) != HAL_I2C_STATE_RESET);
 
-	HAL_I2C_Master_Seq_Transmit_IT(I2C, 0xA1, (uint8_t *)this, SettingsSize, I2C_LAST_FRAME);
+	HAL_I2C_Master_Seq_Transmit_IT(&hi2c1, 0xA0, (uint8_t *)this, SettingsSize, I2C_LAST_FRAME);
 
+	HAL_Delay(100);
 
 	return;
 }
